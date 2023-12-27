ea59ee1611b80414ec4d4809f0000ca5298286c1
Ko Da <63529538+Oyiyi@users.noreply.github.com>
2023-12-26 17:10:27 -0800

    update

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def download_video_and_captions(url, video_title, file_folder):
 
     # Check if the video file exists and download the video if not
     if not os.path.exists(video_file_path):
-        stream = yt.streams.get_highest_resolution()
+        #stream = yt.streams.get_highest_resolution()
+        stream = yt.streams.filter(res="1080p").first()
         print("Downloading video...")
         stream.download(output_path=os.path.join(script_directory, "output", file_folder))
         print("Video downloaded.")
@@ -47,6 +48,6 @@ urls_file = "video_urls.txt"
 with open(urls_file, "r") as file:
     video_urls_list = [line.strip() for line in file.readlines()]
     for url in video_urls_list:
-        print(url)   
-    # url = 'https://www.youtube.com/watch?v=n0Zvshs8wzk&list=PL0eGJygpmOH6JZoTiBexaSQveslxxaLrf&index=23'
+        print(url)
+        # url = 'https://www.youtube.com/watch?v=n0Zvshs8wzk&list=PL0eGJygpmOH6JZoTiBexaSQveslxxaLrf&index=23'
         download_video_and_captions(url, 1, "1")
